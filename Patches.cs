fba51e88567920056175a321f84125fc64d48356
werner <werneralexandre@outlook.com>
2022-04-17 12:30:55 -0300

    init

---
+++ Patches.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Rowing
+{
+    [HarmonyPatch]
+    class Patches
+    {
+		[HarmonyPatch(typeof(Ship), nameof(Ship.GetSailForce))]
+		private class ShipGetSailForce
+		{
+			private static void Postfix(Ship __instance, ref Vector3 __result)
+			{
+				int playersToConsider = __instance.m_players.Count - 1;
+				if (playersToConsider == 0) return;
+
+				int speedMultipler = playersToConsider * Rowing.PercentageToIncreaseSpeedPerPlayer.Value;
+				__result += (__result / 100) * speedMultipler;
+			}
+		}
+	}
+}
---
+++ Rowing.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using UnityEngine;
+using BepInEx.Configuration;
+using HarmonyLib;
+using ServerSync;
+
+namespace Rowing
+{
+    [BepInPlugin(PluginGUID, PluginGUID, Version)]
+    public class Rowing : BaseUnityPlugin
+    {
+        public const string PluginGUID = "Detalhes.Rowing";
+        public const string Name = "Rowing";
+        public const string Version = "1.0.0";
+
+        ConfigSync configSync = new ConfigSync("Detalhes.Rowing") { DisplayName = "Rowing", CurrentVersion = Version, MinimumRequiredVersion = Version };
+
+        Harmony _harmony = new Harmony(PluginGUID);
+
+        public static ConfigEntry<int> PercentageToIncreaseSpeedPerPlayer;
+
+        public void Awake()
+        {
+            PercentageToIncreaseSpeedPerPlayer = config("Server config", "PercentageToIncreaseSpeedPerPlayer", 30,
+                   new ConfigDescription("PercentageToIncreaseSpeedPerPlayer", null));
+            _harmony.PatchAll();
+        }
+
+        ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description, bool synchronizedSetting = true)
+        {
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = configSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        ConfigEntry<T> config<T>(string group, string name, T value, string description, bool synchronizedSetting = true) => config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+
+    }
+}
